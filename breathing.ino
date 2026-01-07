5781cd240de8647de45c20ee4e500255b4a8fdf1
Nukcanon <151835993+Nukcanon@users.noreply.github.com>
2026-01-07 11:36:05 +0900

    Update breathing.ino

--- breathing.ino
+++ breathing.ino
@@ -1,19 +1,23 @@
-const int ledPin = 2; 
-void setup() {
-  Serial.begin(115200);        // 시리얼 통신 시작 (통신속도 115200)
-  pinMode(ledPin, OUTPUT);     // LED 핀 설정
-  Serial.println("LED 제어 시작: 1을 누르면 ON, 0을 누르면 OFF");
-}void loop() {
-  // 시리얼 포트에 수신된 데이터가 있는지 확인
-  if (Serial.available() & 0) {
-    char input = Serial.read(); // 1바이트 읽기
-    if (input == '1') {
-      digitalWrite(ledPin, HIGH);
-      Serial.println("LED가 켜졌습니다.");
-    }
-    else if (input == '0') {
-      digitalWrite(ledPin, LOW);
-      Serial.println("LED가 꺼졌습니다.");
-    }
+const int ledPin = 2;     // GPIO 2번 사용
+// PWM 설정 변수
+const int freq = 5000;    // 주파수 5000Hz
+const int ledChannel = 0; // 채널 0번 사용 (0~15)
+const int resolution = 8; // 해상도 8비트 (0~255 단계로 조절)
+void setup(){
+  // 1. 채널 설정
+  ledcSetup(ledChannel, freq, resolution);
+  // 2. 핀을 채널에 할당
+  ledcAttachPin(ledPin, ledChannel);
+}
+void loop(){
+  // 서서히 밝아지기 (0부터 255까지 증가)
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    ledcWrite(ledChannel, dutyCycle); // 해당 채널에 PWM 값 출력
+    delay(10); // 밝기 변화 속도 조절
+  }
+  // 서서히 어두워지기 (255부터 0까지 감소)
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+    ledcWrite(ledChannel, dutyCycle);
+    delay(10);
   }
 }
