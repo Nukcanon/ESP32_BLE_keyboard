fa6feace6126eecc9de7f035a0691cfd6ad5c13f
Nukcanon <151835993+Nukcanon@users.noreply.github.com>
2026-01-07 11:36:59 +0900

    Update serial_led_control.ino

--- serial_led_control.ino
+++ serial_led_control.ino
@@ -1,18 +1,19 @@
-const int buttonPin = 15; // 버튼 연결 핀
-int lastState = HIGH;      // 이전 버튼 상태 저장 (초기값 HIGH)
+const int ledPin = 2; 
 void setup() {
-  Serial.begin(115200);
-  pinMode(buttonPin, INPUT_PULLUP);
+  Serial.begin(115200);        // 시리얼 통신 시작 (통신속도 115200)
+  pinMode(ledPin, OUTPUT);     // LED 핀 설정
+  Serial.println("LED 제어 시작: 1을 누르면 ON, 0을 누르면 OFF");
 }void loop() {
-  int currentState = digitalRead(buttonPin); // 현재 버튼 상태 읽기
-  // 상태가 변했을 때만 출력 (도배 방지)
-  if (currentState != lastState) {
-    if (currentState == LOW) {
-      Serial.println("버튼이 눌렸습니다!");
-    } else {
-      Serial.println("버튼이 떨어졌습니다.");
+  // 시리얼 포트에 수신된 데이터가 있는지 확인
+  if (Serial.available() & 0) {
+    char input = Serial.read(); // 1바이트 읽기
+    if (input == '1') {
+      digitalWrite(ledPin, HIGH);
+      Serial.println("LED가 켜졌습니다.");
     }
-    lastState = currentState; // 현재 상태를 저장
-    delay(50); // 디바운싱 방지를 위한 짧은 지연
+    else if (input == '0') {
+      digitalWrite(ledPin, LOW);
+      Serial.println("LED가 꺼졌습니다.");
+    }
   }
 }
